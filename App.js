8168debd048845a7415ea6a35e30d258f3a9b94e
Scott <scotty.zifka@gmail.com>
2022-12-16 23:48:52 -0800

    redux attempt

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
-import { Store } from 'redux';
+import { store } from './redux/store';
 
 export default function App() {
     return (
-      <NavigationContainer>
         <Provider store={store}>
-          <Main />
+            <NavigationContainer>
+                <Main />
+            </NavigationContainer>
         </Provider>
-      </NavigationContainer>
-    )
+    );
 }
